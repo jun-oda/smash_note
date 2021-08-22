ff60e3265b60f943b7c0b4798ca150d16b1366f5
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-08-22 17:41:29 +0900

    画像を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_021110) do
+ActiveRecord::Schema.define(version: 2021_08_20_041825) do
+
+  create_table "user_notes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "content1"
+    t.string "content2"
+    t.string "content3"
+    t.string "content4"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
