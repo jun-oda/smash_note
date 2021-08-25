38b23d689bf40ded388dd0404808c72304979c7c
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-08-25 10:58:10 +0900

    usernote_controller追加前

--- db/migrate/20210824110313_add_userid_to_characters.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToCharacters < ActiveRecord::Migration[6.1]
-  def change
-    add_column :characters, :user_id, :integer
-  end
-end
--- db/migrate/20210824110530_add_content1_to_characters.rb
+++
@@ -1,5 +0,0 @@
-class AddContent1ToCharacters < ActiveRecord::Migration[6.1]
-  def change
-    add_column :characters, :content1, :text
-  end
-end
